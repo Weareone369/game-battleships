5e417f352660506606e82abc0c1cc0bbf1e917b4
Olga Molinaro <sophia_ballare@hotmail.co.uk>
2024-11-17 15:52:55 +0000

    Create functionality for adding ships to the board

--- grid.py
+++ grid.py
@@ -7,6 +7,18 @@ class Grid:
         self.ships = [] 
         self.grid = [['~' for _ in range(size)] for _ in range(size)] 
 
+        
+    def add_ship(self, ship): 
+        """ Add a ship to the grid if placement is valid. A ship is defined as a list of (x, y) tuples representing its coordinates. """ 
+        for x, y in ship: 
+            if not (0 <= x < self.size and 0 <= y < self.size): 
+                raise ValueError("Ship is out of bounds.") 
+            if self.grid[x][y] != '~': 
+                raise ValueError("Ship overlaps with another ship.") 
+                
+        self.ships.append(ship) 
+        for x, y in ship: self.grid[x][y] = 'S' 
+         
     def display(self, reveal=False):
         """Display the grid, Reveal ship's if 'reveal' is True."""
         for row in self.grid:
